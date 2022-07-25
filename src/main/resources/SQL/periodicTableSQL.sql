387bd8dff644d2b457dd9aa5d09e3563020820f2
nathanwmeyer <84550580+nathanwmeyer@users.noreply.github.com>
2022-07-24 20:10:50 -0700

    Create periodicTableSQL.sql

---
+++ src/main/resources/SQL/periodicTableSQL.sql
@@ -0,0 +1,84 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: periodic
+-- ------------------------------------------------------
+-- Server version	5.7.24
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `elements`
+--
+
+DROP TABLE IF EXISTS `elements`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `elements` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `ELEMENT_NAME` varchar(55) NOT NULL,
+  `ATOMIC_NUM` int(11) NOT NULL,
+  `FORM` varchar(55) NOT NULL,
+  `DESCRIPTION` text NOT NULL,
+  `RADIOACTIVE` varchar(45) NOT NULL,
+  `PRICE` double NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8 COMMENT='table for periodic table of elements';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `elements`
+--
+
+LOCK TABLES `elements` WRITE;
+/*!40000 ALTER TABLE `elements` DISABLE KEYS */;
+INSERT INTO `elements` VALUES (43,'Hydrogen',1,'Gas','A flammable gas. The LZ 129 Hindenburg was filled with hydrogen, which lead to its combustion.','YES',1.5);
+/*!40000 ALTER TABLE `elements` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `purchases`
+--
+
+DROP TABLE IF EXISTS `purchases`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `purchases` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `PURCHASER` varchar(45) NOT NULL,
+  `ITEM_ID` int(11) NOT NULL,
+  `ITEM_NAME` varchar(45) NOT NULL,
+  `QUANTITY` int(11) NOT NULL,
+  `TOTAL_COST` double NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `purchases`
+--
+
+LOCK TABLES `purchases` WRITE;
+/*!40000 ALTER TABLE `purchases` DISABLE KEYS */;
+INSERT INTO `purchases` VALUES (1,'User1',43,'Hydrogen',5,7.5);
+/*!40000 ALTER TABLE `purchases` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-07-24 19:35:29
